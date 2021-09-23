9f471c2baae4a5121f142d4549539cb4c9555b04
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-22 21:34:51 -0300

    Starting icon customization in TopBar.css

--- src/TopBar/TopBar.jsx
+++ src/TopBar/TopBar.jsx
@@ -3,7 +3,10 @@ import "./TopBar.css"
 export default function TopBar(){
 
     return(
-        <div className="Top">            
+        <div className="Top">
+            <div className="topLeft"><i class="fab fa-facebook-square"></i></div>            
+            <div className="topCenter">C</div>            
+            <div className="topRight">R</div>            
             
         </div>
 
