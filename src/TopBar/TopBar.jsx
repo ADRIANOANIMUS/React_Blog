3da93bad5765adc0fb9121a5251ed9ad982a4a64
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-26 16:48:36 -0300

    Creating options for users

--- src/TopBar/TopBar.jsx
+++ src/TopBar/TopBar.jsx
@@ -10,7 +10,14 @@ export default function TopBar(){
             <i class="fab fa-pinterest"></i>
             <i class="fab fa-instagram"></i>
             </div>            
-            <div className="topCenter">C</div>            
+            <div className="topCenter">
+                <ul className="topList"></ul>
+                    <li className="topListItem">HOME</li>
+                    <li className="topListItem">ABOUT</li>
+                    <li className="topListItem">CONTACT</li>
+                    <li className="topListItem">WRITE</li>
+                    <li className="topListItem">LOGOUT</li>
+            </div>            
             <div className="topRight">R</div>            
             
         </div>
