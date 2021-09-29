293bc6db9592f5c3fc2b6d73531e592036050c7d
AdrianoAnimus <adrianoanimus@gmail.com>
2021-09-28 21:35:23 -0300

    Creating the Sidebar List and editing Sidebar CSS

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -2,9 +2,34 @@ import "./sidebar.css"
 
 export default function SideBar() {
     return (
-        <div className ="sidebar">
-            sidebar
-            
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUTE ME</span>
+                <img src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg" alt="" />
+
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat voluptates deleniti error tenetur dolores aspernatur
+                    voluptatem delectus dicta accusantium nostrum.
+                </p>
+                <div className="sidebarItem"></div>
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
         </div>
     )
 }
