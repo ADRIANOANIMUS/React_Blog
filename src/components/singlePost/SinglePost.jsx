32bec1d24423b492625259be958d6cb2215c28f6
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-05 10:04:19 -0300

    Create Icons to Delete and Edit

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -2,14 +2,19 @@ import "./singlePost.css"
 
 export default function SinglePost() {
     return (
-        <div className ="singlePost">
+        <div className="singlePost">
             <div className="singlePostWrapper">
-            <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
-            alt="" className="singlePostImg" 
-            />
-            
+                <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    alt="" className="singlePostImg"
+                />
+                <h1 className="singlePostTitle">Lorem ipsum dolor sit amet
+                    <div className="singlePostEdit">
+                        <i class="far fa-edit"></i>
+                        <i class="fas fa-trash-alt"></i>
+                    </div>
+                </h1>
 
-        </div>
+            </div>
         </div>
     )
 }
