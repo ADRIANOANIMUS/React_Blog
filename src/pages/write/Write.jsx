b423e6ff1ba2256a0105ed8772206615fafa80fd
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-05 22:47:54 -0300

    Editing write.jsx

--- src/pages/write/Write.jsx
+++ src/pages/write/Write.jsx
@@ -5,10 +5,18 @@ export default function Write() {
         <div className = "write">
             <form className ="writeForm">
                 <div className="writeFormGroup">
-                    <label htmlFor="fileInput"></label>
-                    <input type="file" id="fileInput" />
-                    <input type="text" placeholder ="Title" />
+                    <label htmlFor="fileInput">
+                    <i class="fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: "none" }}/>
+                    <input type="text" placeholder ="Title" className ="writeIput" autoFocus={true} />
                 </div>
+                <div className="writeFormGroup">
+                    <textarea placeholder ="Tell your story..." type ="text" className ="writeIput writeText">
+
+                    </textarea>
+                </div>
+                <button className="writeSubmit">Publish</button>
             </form>
         </div>
     )
