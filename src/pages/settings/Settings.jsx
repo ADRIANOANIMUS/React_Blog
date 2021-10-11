a74ba64669eca6565810a099c2824bdf4b6043d2
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-11 19:21:28 -0300

    Create Sidebar settings

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,9 +1,15 @@
 import "./settings.css"
+import Sidebar from "../../components/sidebar/Sidebar"
 
 export default function Settings() {
     return (
         <div className ="settings">
-            
+            <div className="settingsWrapper"></div>
+            <div className="settingsTitle">
+                <span className="settingsUpdateTitle">Update your cout</span>
+                <span className="settingsUpdateTitle">Delete your count</span>
+            </div>
+            <Sidebar/>
         </div>
     )
 }
