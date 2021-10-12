f34d55a8d722989e5834c717db257bad85f428a5
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-12 20:41:01 -0300

    Updating Settings

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -13,22 +13,22 @@ export default function Settings() {
                 <form className="settingsForm">
                     <label> Profile Picture </label>
                     <div className="settingsPP">
-
-                        <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" 
+                        <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""
                         />
                         <label htmlFor="fileInput">
                             <i className="settingsPPIcon far fa-user-circle"></i>{""}
                         </label>
-                        <input type="file" id="fileInput" style={{ display: "none" }} />
+                        <input type="file" id="fileInput" style={{ display: "none" }} className="settingsPPInput" />
                     </div>
                     <label>Username</label>
-                    <input type="text" placeholder="Adriano" />
+                    <input type="text" placeholder="Adriano" name="name" />
                     <label>Email</label>
                     <input type="email" placeholder="adrianoanimus@gmail.com" name="email" />
                     <label>Password</label>
-                    <input type="password" />
-                    <button className="settingsSubmit">Update</button> 
-
+                    <input type="password" placeholder="Password" name="password" />
+                    <button className="settingsSubmit">
+                        Update
+                    </button>
                 </form>
             </div>
             <Sidebar />
