cb6095456c3d5e0afedf75d19fdfce98d3309dd2
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-21 22:14:50 -0300

    Create and edith mongoose connection

--- api/index.js
+++ api/index.js
@@ -5,6 +5,16 @@ const mongoose = require("mongoose")
 
 dontev.config()
 
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(console.log("connected to MongoDB"))
+.catch((err)=>console.log(err))
+
+
 app.listen("5000" , () => {
     console.log("Backend application")
 })
