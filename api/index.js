bdf871307b960d2e77e13b841a2a375594d8b207
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-21 21:54:53 -0300

    Create const mongoose and dontev

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
+const dontev = require ("dontev")
+const mongoose = require("mongoose")
+
+dontev.config()
 
 app.listen("5000" , () => {
-    console.log("Backend")
+    console.log("Backend application")
 })
