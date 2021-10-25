c208c8a252129aa84c6f32d39c93a9612dd98d06
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-25 18:57:23 -0300

    Creating Posts Data

---
+++ api/models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: false
+    },
+    username: {
+        type: String,
+        required: false
+    },
+    categories: {
+        type: Array,
+        require: false
+    }
+},
+    { timestamps: true }
+)
+
+module.exports = mongoose.model("Post", PostSchema)
