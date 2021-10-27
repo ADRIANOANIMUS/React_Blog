2efc89a878728f8cbdf9366a793fca6b4c101a03
AdrianoAnimus <adrianoanimus@gmail.com>
2021-10-26 21:05:06 -0300

    Create conditions for user password

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,7 +7,14 @@ const bcrypt = require("bcrypt")
 router.post("/:id", async (req, res) => {
 
     if (req.body.userId === req.params.id) {
+        if(req.body.password){
+            const salt = await bcrypt.hash(req.body.password, salt)
+        }
         try {
+            const updateUser = await User.findByIdAndUpdate(req.params.id,{
+                $set:req.body,
+            })
+            res.status(200).json(updateUser)
         } catch (err) {
             res.status(500).json(err)
         }
