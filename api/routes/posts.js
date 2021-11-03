2e94d959d39fa72c9e78358d378daaf5269e45ba
AdrianoAnimus <adrianoanimus@gmail.com>
2021-11-03 19:15:55 -0300

    Create router Put

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -7,29 +7,48 @@ const Post = require("../models/Post")
 
 router.post("/", async (req, res) => {
     const newPost = new Post(req.body)
-    try{
+    try {
         const savedPost = await newPost.save()
         res.status(200).json(savedPost)
-    }catch(err){
+    } catch (err) {
         res.status(500).json(err)
     }
 
 })
 
 //UPDATE POST
-router.delete("/:id", async (req, res) => {
-        
+router.put("/:id", async (req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdated(req.params.id, {
+                    $set: req.body
+                }, { new: true }
+                )
+                res.status(200).json(updatedPost)
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        } else {
+            res.status(401).json("You can update only your post!")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
 })
 
 //DELETE POST
 
 //GET POST
 router.get("/:id", async (req, res) => {
-    try{
+    try {
         const user = await User.findByIdAndDelete(req.params.id)
-        const {password, ...others} = user._doc
-        res.status(200).json(others)        
-    }catch (err) {
+        const { password, ...others } = user._doc
+        res.status(200).json(others)
+    } catch (err) {
         res.status(500).json(err)
     }
 })
