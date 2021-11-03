8e70fb26bfc66e163c43fbfb3b62052b57f78e90
AdrianoAnimus <adrianoanimus@gmail.com>
2021-11-03 19:29:32 -0300

    Create router Delete

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -42,15 +42,37 @@ router.put("/:id", async (req, res) => {
 
 //DELETE POST
 
+router.delete("/:id", async (req, res) => {
+
+    try {
+        const post = await Post.findById(req.params.id)
+        if (post.username === req.body.username) {
+            try {
+                await post.delete()
+                res.status(200).json("Post has been Deleted...")
+            } catch (err) {
+                res.status(500).json(err)
+
+            }
+        } else {
+            res.status(401).json("You can delete only your post!")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+
 //GET POST
 router.get("/:id", async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        const { password, ...others } = user._doc
-        res.status(200).json(others)
+        const post = await Post.findById(req.params.id)        
+        res.status(200).json(post)
     } catch (err) {
         res.status(500).json(err)
     }
 })
+
+
 
 module.exports = router
